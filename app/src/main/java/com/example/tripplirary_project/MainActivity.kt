05a5c823c8942cb5aa7ad2f4de0af698b82959fe
Ji <74065815+Jiheon-Lee98@users.noreply.github.com>
2023-07-24 17:17:17 +0900

    BottomNavi_Fragment Init

--- app/src/main/java/com/example/tripplirary_project/MainActivity.kt
+++ app/src/main/java/com/example/tripplirary_project/MainActivity.kt
@@ -1,19 +1,87 @@
 package com.example.tripplirary_project
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.tripplirary_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var activityMainBinding: ActivityMainBinding
+    companion object {
+        val HOME_FRAGMENT = "HomeFragment"
+        val POST_FRAGMENT = "PostFragment"
+        val PLAN_FRAGMENT = "PlanFragment"
+        val SHARE_FRAGMENT = "ShareFragment"
+        val MY_FRAGMENT = "MyFragment"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         activityMainBinding.run {
-
+            bnViewMain.run {
+                setOnItemSelectedListener {
+                    when (it.itemId){
+                        R.id.itemHome -> {
+                            replaceFragment(HOME_FRAGMENT, false ,false)
+                        }
+                        R.id.itemPost -> {
+                            replaceFragment(POST_FRAGMENT, false ,false)
+                        }
+                        R.id.itemPlan -> {
+                            replaceFragment(PLAN_FRAGMENT, false ,false)
+                        }
+                        R.id.itemShare -> {
+                            replaceFragment(SHARE_FRAGMENT, false ,false)
+                        }
+                        R.id.itemMy -> {
+                            replaceFragment(MY_FRAGMENT, false ,false)
+                        }
+                    }
+                    true
+                }
+            }
         }
 
         setContentView(activityMainBinding.root)
+    }
+    fun replaceFragment(name: String, addToBackStack: Boolean, animate: Boolean){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var newFragment = when(name){
+            HOME_FRAGMENT -> {
+                HomeFragment()
+            }
+            POST_FRAGMENT -> {
+                PostFragment()
+            }
+            PLAN_FRAGMENT -> {
+                PlanFragment()
+            }
+            SHARE_FRAGMENT -> {
+                ShareFragment()
+            }
+            MY_FRAGMENT -> {
+                MyFragment()
+            }
+            else -> {
+                Fragment()
+            }
+        }
+
+        fragmentTransaction.replace(R.id.fcvMain, newFragment)
+        if (animate) {
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        }
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(name)
+        }
+        fragmentTransaction.commit()
+    }
+
+    fun removeFragment(name:String){
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
---
+++ app/src/main/java/com/example/tripplirary_project/PlanFragment.kt
@@ -0,0 +1,28 @@
+package com.example.tripplirary_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tripplirary_project.databinding.FragmentPlanBinding
+
+class PlanFragment : Fragment() {
+
+    lateinit var fragmentPlanBinding: FragmentPlanBinding
+    lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentPlanBinding = FragmentPlanBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        fragmentPlanBinding.run {
+
+        }
+
+        return fragmentPlanBinding.root
+    }
+}
