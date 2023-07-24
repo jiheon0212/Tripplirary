c78bc6900587559faa8b070a869cfe7470fdeb76
Ji <74065815+Jiheon-Lee98@users.noreply.github.com>
2023-07-24 17:40:56 +0900

    PlanFragment AddBtn1

--- app/src/main/java/com/example/tripplirary_project/PlanAddFragment.kt
+++ app/src/main/java/com/example/tripplirary_project/PlanAddFragment.kt
@@ -24,6 +24,7 @@ class PlanAddFragment : Fragment() {
                 title = "Pre_PlanAdd"
 
                 // backstack 에서 해당 fragment 삭제 후 이전으로 복귀
+                //todo : 돌아가기전에 사용자에게 dialog로 내용 저장 안된다는 것 고지하기
                 setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
                 setNavigationOnClickListener{
                     mainActivity.removeFragment(MainActivity.PLAN_ADD_FRAGMENT)
